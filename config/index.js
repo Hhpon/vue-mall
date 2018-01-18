ee6be2a6f9a9bf8e5284cd9b3a75ebb7587481a3
李嘉俊 <794857535@qq.com>
2018-01-18 16:48:11 +0800

    修改本地代理
    修改本地代理

--- config/index.js
+++ config/index.js
@@ -5,38 +5,38 @@
 const path = require('path')
 
 module.exports = {
+    
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
          //下面代理无需本地服务端
-        '/goods':{
-            target:'http://120.78.185.163:3000'
-        },
-        '/goods/*':{
-            target:'//http://120.78.185.163:3000'
-        },
-        '/users/**':{
-            target:'http://120.78.185.163:3000'
-        },
-        '/admin/**':{
-            target:'http://120.78.185.163:3000'
-        }
-        //下面代理需要依赖本地服务端
         // '/goods':{
-        //     target:'http://localhost:3000'
+        //     target:'http://120.78.185.163:3000'
         // },
         // '/goods/*':{
-        //     target:'http://localhost:3000'
+        //     target:'//http://120.78.185.163:3000'
         // },
         // '/users/**':{
-        //     target:'http://localhost:3000'
+        //     target:'http://120.78.185.163:3000'
         // },
         // '/admin/**':{
-        //     target:'http://localhost:3000'
+        //     target:'http://120.78.185.163:3000'
         // }
+        //下面代理需要依赖本地服务端
+        '/goods':{
+            target:'http://localhost:3000'
+        },
+        '/goods/*':{
+            target:'http://localhost:3000'
+        },
+        '/users/**':{
+            target:'http://localhost:3000'
+        },
+        '/admin/**':{
+            target:'http://localhost:3000'
+        }
     },
 
     // Various Dev Server settings
